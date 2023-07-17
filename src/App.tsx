eec2e8e92937471840265166882a237a971dd98a
seoyeon <wjstjdus96@naver.com>
2023-07-17 21:43:22 +0900

    feat: 페이지 헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { styled } from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  div {
+    margin-bottom: 15px;
+    font-size: 30px;
+    font-weight: 800;
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header>
+        <div>국내 모든 임상시험 검색하고</div>
+        <div>온라인으로 참여하기</div>
+      </Header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { styled, createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body{
+    background-color: #D0E8FD;
+    padding: 40px;
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </>,
 );
